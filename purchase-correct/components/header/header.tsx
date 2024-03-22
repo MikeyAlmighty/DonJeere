a6280d4e321f95f8ecf29b9bdae3cc88f990ce98
MikeyAlmighty <mike.leibbrandt@gmail.com>
2024-03-22 17:56:40 +0200

    Init project + base configuration

---
+++ purchase-correct/components/header/header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = (props) => {
+
+    return(
+        <nav>
+            <li></li>
+            <h6></h6>
+        </nav>
+    )
+
+}
+
+export default Header;
