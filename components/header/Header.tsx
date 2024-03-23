8ce79810170f0637e6b25c3da9a8c6eb51cfe408
MikeyAlmighty <mike.leibbrandt@gmail.com>
2024-03-23 16:00:35 +0200

    feat: Added (literal) Easter egg

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,19 +2,25 @@ import React from 'react';
 import Link from "next/link";
 
 import './Styles.scss';
+import {EasterEgg} from "../easter-egg/EasterEgg";
+import {toast} from "react-toastify";
 
-export const Header = () => (
-    <header>
-        <h1>Don Jeere</h1>
-        <nav>
-            <ul>
-                <Link className='link' href="/">Home</Link>
-                <Link className='link' href="#what-we-do">What We Do</Link>
-                <Link className='link' href="#about">About</Link>
-                <Link className='link' href="#team">Our Team</Link>
-                <Link className='link' href="#gallery">Gallery</Link>
-                <Link className='link' href="#contact-us">Contact Us</Link>
-            </ul>
-        </nav>
-    </header>
-)
+export const Header = () => {
+    const notify = () => toast("Happy Easter!");
+    return(
+        <header>
+            <h1>Don Jeere</h1>
+            <nav>
+                <ul>
+                    <Link className='link' href="/">Home</Link>
+                    <Link className='link' href="#what-we-do">What We Do</Link>
+                    <Link className='link' href="#about">About</Link>
+                    <Link className='link' href="#team">Our Team</Link>
+                    <Link className='link' href="#gallery">Gallery</Link>
+                    <Link className='link' href="#contact-us">Contact Us</Link>
+                    <EasterEgg handleClick={notify} />
+                </ul>
+            </nav>
+        </header>
+    )
+}
